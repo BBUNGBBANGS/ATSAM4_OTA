f7409246f0c244b325662b389bdc2c8cada8fff0
jisu.seo <susjs90@gmail.com>
2023-04-07 21:30:42 +0900

    개발완료

--- ATSAM4_OTA/src/main.c
+++ ATSAM4_OTA/src/main.c
@@ -3,11 +3,11 @@
 #include "xmodem.h"
 #include "flash.h"
 
-volatile uint32_t *my_var = (uint32_t *)0x20001000;
-
+volatile uint32_t * const Reprogram_Info = (uint32_t *)0x0041FFF8;
 int main(void)
 {
 	uint8_t buf[100] = {0,};
+	uint32_t Reprogram_Buffer[2] = {0,};
 	uint32_t *Application_Address = (uint32_t *)FLASH_APP1_START_ADDRESS;
 	uint32_t temp;
 	/* Initialize the board. */
@@ -17,7 +17,8 @@ int main(void)
 	Boot_Uart_Init();
 	delay_ms(10);
 	temp = *Application_Address;
-	if ((*Application_Address != 0xFFFFFFFFu) && (*Application_Address != 0x00000000u))
+	uint32_t Reprogram_Info_Data = *Reprogram_Info;
+	if ((*Application_Address != 0xFFFFFFFFu) && (*Application_Address != 0x00000000u) && (Reprogram_Info_Data != 0x01))
 	{
 		Flash_Jump_To_Application();
 	}
@@ -29,7 +30,13 @@ int main(void)
 	Uart_Transmit_Str(buf);
 	sprintf((char *)buf,"\r\n\r\nErase App2 Flash Section\r\n");
 	Uart_Transmit_Str(buf);
-	Flash_Erase(FLASH_APP2_START_ADDRESS,FLASH_COPY_SIZE/FLASH_PAGE_SIZE);
+	Flash_Erase(FLASH_APP2_START_ADDRESS,104);
+	flash_unlock(0x0041FFF8, 0x0041FFF8 + 8 - 1, 0, 0);
+	delay_ms(10);
+	flash_write(0x0041FFF8, Reprogram_Buffer, 8, 0);
+	delay_ms(10);
+	flash_lock(0x0041FFF8, 0x0041FFF8 + 8 - 1, 0, 0);
+	delay_ms(10);
 	while(1)
 	{
 		sprintf((char *)buf,"Please send a new binary file with Xmodem protocol to update the firmware.\r\n");
@@ -37,5 +44,6 @@ int main(void)
 		xmodem_receive();		
 		sprintf((char *)buf,"\r\nFailed... Please try again.\r\n");
 		Uart_Transmit_Str(buf);
+		Flash_Jump_To_Application();
 	}
 }
--- ATSAM4_OTA/src/main.h
+++ ATSAM4_OTA/src/main.h
@@ -7,12 +7,5 @@
 #include <stdbool.h>
 #include "asf.h"
 
-typedef struct 
-{
-    uint8_t Reprogram_Flag;
-    uint8_t dummy1;
-    uint8_t dummy2;
-    uint8_t dummy3;
-}Reprogram_Info_t;
 
 #endif
